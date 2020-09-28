b0693bf874ce991f391d76b541de6e9522fe6b0e
HaoranzhuExplorer <haoranzhux@gmail.com>
2020-09-27 22:42:03 -0400

    update

---
+++ main.py
@@ -0,0 +1,14 @@
+from plot import get_diff_jpeg_df, get_guetzli_df
+import os
+import io
+import time
+
+
+time_start = time.time()
+#get_guetzli_df(directory="/Users/Haoran/Desktop/data/raw512", effective_bytes=False)
+get_diff_jpeg_df(directory="/Users/Haoran/Desktop/data/raw512", effective_bytes=False)
+time_end = time.time()
+duration = time_end - time_start
+print("duration: "+str(duration)+" seconds")
+
+
---
+++ time_test.py
@@ -0,0 +1,5 @@
+import lpips
+import os
+
+os.system("python3 lpips_2imgs.py -p0 /Users/Haoran/Desktop/data/raw512/md0d3ca2e738.png_99_compressed.jpeg  -p1  /Users/Haoran/Desktop/data/raw512/md0d3ca2e738.png")
+
