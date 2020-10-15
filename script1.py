1077cab65fda28324fbb76af4d16de1a15bed2f2
HaoranzhuExplorer <haoranzhux@gmail.com>
2020-10-15 19:33:49 -0400

    update

--- script1.py
+++
@@ -1,4 +0,0 @@
-from plot import get_diff_jpeg_df, get_guetzli_df
-
-dataset1 = "/home/Haoran/distortion_rate/temp/kodak512"
-get_guetzli_df(directory=dataset1, effective_bytes=False, write_files=True)
--- time_test.py
+++
@@ -1,5 +0,0 @@
-import lpips
-import os
-
-os.system("python3 lpips_2imgs.py -p0 /Users/Haoran/Desktop/data/raw512/md0d3ca2e738.png_99_compressed.jpeg  -p1  /Users/Haoran/Desktop/data/raw512/md0d3ca2e738.png")
-
